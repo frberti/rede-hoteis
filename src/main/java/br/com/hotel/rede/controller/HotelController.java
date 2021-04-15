4c06828e3dbd9ee7c1741f7b5fa39980a30d5ff8
Francisco Berti <berti.ffrancisco@gmail.com>
2021-04-15 17:09:55 -0300

    criacao de persistencias do hotel

--- src/main/java/br/com/hotel/rede/controller/HotelController.java
+++ src/main/java/br/com/hotel/rede/controller/HotelController.java
@@ -2,6 +2,7 @@ package br.com.hotel.rede.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,30 +10,34 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.hotel.rede.model.Hotel;
+import br.com.hotel.rede.repository.HotelRepository;
 
 @RestController
 @RequestMapping("/hotel")
 public class HotelController {
+	
+	@Autowired
+	HotelRepository hotelRepository;
 
 	@GetMapping
 	public List<Hotel> buscar() {
 		System.out.println("buscando hoteis");
-		return null;
+		return hotelRepository.findAll();
 	}
 
 	@GetMapping("/{id}")
 	public Hotel buscarPorId(@PathVariable Long id) {
 		System.out.println("buscando hotel por id" + id);
-		return null;
+		return hotelRepository.findById(id).get();
 	}
 
 	@PostMapping
 	public void gravar(@RequestBody Hotel hotel) {
 		System.out.println(hotel);
+		hotelRepository.save(hotel);
 	}
 	
 	@PutMapping("/{id}")
--- src/main/java/br/com/hotel/rede/model/Hotel.java
+++ src/main/java/br/com/hotel/rede/model/Hotel.java
@@ -2,6 +2,7 @@ package br.com.hotel.rede.model;
 
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
@@ -10,9 +11,9 @@ import javax.persistence.OneToOne;
 public class Hotel extends BaseModel {
 
 	private String nome;
-	@OneToOne
+	@OneToOne(cascade = CascadeType.ALL)
 	private Endereco endereco;
-	@OneToMany (mappedBy = "hotel")
+	@OneToMany (mappedBy = "hotel", cascade = CascadeType.ALL)
 	private List<Quarto> quartos;
 	
 
