f5adc712e5aa14a09e6d654dd73053a4f5e235e1
Francisco Berti <berti.ffrancisco@gmail.com>
2021-04-15 13:44:53 -0300

    projeto inicial

---
+++ src/main/java/br/com/hotel/rede/RedeApplication.java
@@ -0,0 +1,13 @@
+package br.com.hotel.rede;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedeApplication.class, args);
+	}
+
+}
